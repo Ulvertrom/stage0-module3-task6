52a0477fb520611f1552f649ac12a10fdd219dfd
ulvertrom <ulvertrom@gmail.com>
2023-02-09 22:22:00 +0400

    added 4 progs

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,14 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public static void roundNumber(float numberToBeRounded) {
+        int roundInt = (int) numberToBeRounded;
+        numberToBeRounded = numberToBeRounded - roundInt;
+        numberToBeRounded = numberToBeRounded * 10;
+        numberToBeRounded = (int) (numberToBeRounded / 5);
+        numberToBeRounded = numberToBeRounded + roundInt;
+        System.out.println((int) numberToBeRounded);
+
     }
+
 }
